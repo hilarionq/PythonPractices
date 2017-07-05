ba205d851c41c7189bd972396c984aa5af60ce2b
hilarionq <hilarionq@gmail.com>
2017-07-05 15:26:05 -0400

    Adding ContactList class to check the extending built-ins on python

--- ContactsApp/contacts.py
+++ ContactsApp/contacts.py
@@ -1,6 +1,15 @@
+class ContactList(list):
+    def search(self, name):
+        '''Return all contacts that contain the search value in their name.'''
+        matching_contacts = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts
+
 class Contact:
     '''Contact class definition'''
-    all_contacts = []
+    all_contacts = ContactList()
     def __init__(self, name, email):
         self.name = name
         self.email = email
