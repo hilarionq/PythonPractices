f3f11291f43d966dca3c530094dcd46e72b672e2
hilarionq <hilarionq@gmail.com>
2017-07-05 16:08:47 -0400

    Adding LongDictName class to see the extension of built-ins (dictionary)

---
+++ LongNameDict.py
@@ -0,0 +1,14 @@
+class LongNameDict(dict):
+    def longest_key(self):
+        longest = None
+        for key in self:
+            if not longest or len(key) > len(longest):
+                longest = key
+        return longest
+
+#Testing the class
+longkeys = LongNameDict()
+longkeys['hello'] = 1
+longkeys['longest yet'] = 5
+longkeys['hello2'] = 'world'
+print(longkeys.longest_key()) #-> 'longest yet'
