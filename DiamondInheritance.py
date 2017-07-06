0b9ce046cec13ca0200d1673d13fdac020a3c8c3
hilarionq <hilarionq@gmail.com>
2017-07-06 15:27:54 -0400

    Replace calls to call_me methods for Super calls, in order to avoid the Diamond Inheritance with python.

--- DiamondInheritance.py
+++ DiamondInheritance.py
@@ -7,22 +7,20 @@ class BaseClass:
 class LeftSubClass(BaseClass):
     num_left_calls = 0
     def call_me(self):
-        BaseClass.call_me(self)
+        super().call_me()
         print("Calling method on Left Subclass")
         self.num_left_calls += 1
 
 class RightSubClass(BaseClass):
     num_right_calls = 0
     def call_me(self):
-        BaseClass.call_me(self)
+        super().call_me()
         print("Calling method on right Subclass")
         self.num_right_calls += 1
 
 class SubClass(LeftSubClass, RightSubClass):
     num_sub_calls = 0
     def call_me(self):
-        LeftSubClass.call_me(self)
-        RightSubClass.call_me(self)
+        super().call_me()
         print ("Calling method on Subclass")
         self.num_sub_calls += 1
-
