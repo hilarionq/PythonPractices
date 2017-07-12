47bc21f573614da23340185cb84c3b23646854f4
hilarionq <hilarionq@gmail.com>
2017-07-12 15:32:14 -0400

    The ZipSearch class refactored in order to use the new  ZipProcessor class

--- zip_search.py
+++ zip_search.py
@@ -1,40 +1,21 @@
 import sys
-import shutil
-import zipfile
+import os
+from zip_processor import ZipProcessor
 
-from pathlib import Path
-
-class ZipReplace:
+class ZipReplace(ZipProcessor):
     def __init__(self, filename, search_string, replace_string):
-        self.filename = filename
+        super().__init__(filename)
         self.search_string  = search_string
         self.replace_string = replace_string
-        self.temp_directory = Path("unzipped - {}".format(filename))
 
-    def zip_find_replace(self):
-        self.unzip_files()
-        self.find_replace()
-        self.zip_files()
-
-    def unzip_files(self):
-        self.temp_directory.mkdir()
-        with zipfile.ZipFile(self.filename) as zip:
-            zip.extractall(str(self.temp_directory))
-    
-    def find_replace(self):
+    def process_files(self):
+        ''' Perform a search and replace on all files in the temporary directory.'''
         for filename in self.temp_directory.iterdir():
             with filename.open() as file:
                 contents = file.read()
-            contents = contents.replace(
-                self.search_string, self.replace_string)
+            contents = contents.replace(self.search_string, self.replace_string)
             with filename.open("w") as file:
-                file.write(contents)
-    
-    def zip_files(self):
-        with zipfile.ZipFile(self.filename,'w') as file:
-            for filename in self.temp_directory.iterdir():
-                file.write(str(filename), filename.name)
-        shutil.rmtree(str(self.temp_directory))
+                file.write(contents)   
 
 if __name__ == '__main__':
-    ZipReplace(*sys.argv[1:4]).zip_find_replace()
+    ZipReplace(*sys.argv[1:4]).process_zip()
